ab2e67fa89f21229884b263526a12fea87d841b3
davidvdw02 <davidvdw02@gmail.com>
2024-02-02 14:55:41 +0100

    fixed building bug

--- src/app/admin-portal/add-product/add-product.service.ts
+++ src/app/admin-portal/add-product/add-product.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { data } from 'cypress/types/jquery';
 
 @Injectable({
     providedIn: 'root'
