619aaebc7ffe79b6ed94abd588419f08dcd72ad5
davidvdw02 <davidvdw02@gmail.com>
2024-02-04 02:16:57 +0100

    Refactor add-product component and service

--- src/app/admin-portal/add-product/add-product.service.ts
+++ src/app/admin-portal/add-product/add-product.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Category } from 'src/app/interfaces/category.interface';
 @Injectable({
     providedIn: 'root'
 })
 export class AddProductService {
+  private dataSubject = new Subject<Category>();
+  public onCategorySave$: Observable<Category> = this.dataSubject.asObservable();
     private apiUrl = environment.apiUrl;
     constructor(private http: HttpClient) { }
 
     addProduct(product: any){
-        console.log(product);
-      this.http.post(this.apiUrl+'/products', product).subscribe(data => console.log(data));
+      this.http.post(this.apiUrl+'products', product).subscribe();
+    }
+
+    getAllCategories(){
+        return this.http.get(this.apiUrl+'categories');
+    }
+    addCategories(category: any){
+        this.http.post(this.apiUrl+'categories', category).subscribe(data => this.dataSubject.next(data as Category));
     }
 }
--- src/app/admin-portal/admin-portal.module.ts
+++ src/app/admin-portal/admin-portal.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { AdminPortalComponent } from './admin-portal.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { FormsModule } from '@angular/forms';
+import { AddCategoryDialogComponent } from './add-product/add-category-dialog/add-category-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 
 @NgModule({
   declarations: [
     AdminPortalComponent,
-    AddProductComponent
+    AddProductComponent,
+    AddCategoryDialogComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
+    MatDialogModule,
   ],
   exports:[AdminPortalComponent]
 })
